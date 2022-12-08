01a894fd1487d6e3cfc537557024e73e87a0cb0e
Maralaanaa <gansukh.mapalmaa@gmail.com>
2022-12-08 18:12:24 +0800

    B200910040

--- App.js
+++ App.js
@@ -1,20 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
+import React, { useRef, useEffect } from 'react'; 
+import { Animated, Text, View } from 'react-native';
+const FadeInView = (props) => {
+const fadeAnim = useRef(
+  new Animated.Value(0)).current //Initial value for opacity: 0
+  React.useEffect(() => { Animated.timing(
+    fadeAnim, {
+      toValue: 1,
+      duration: 10000,
+    }
+    ).start();
+  }, [fadeAnim])
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  <Animated.View
+  style={{
+    ...props.style,
+    opacity: fadeAnim,
+    }
+    }>
+      {props.children}
+      </Animated.View>
+      ); 
+    }
+    export default () => {
+      return (
+      <View style={{
+        flex: 1, 
+        alignItems: 'center', 
+        justifyContent:'center'
+        }
+        }>
+          <FadeInView style={{
+            width: 250, 
+            height: 50,
+            backgroundColor: 'powderblue'
+            }
+            }>
+              <Text style={{
+                fontSize: 28, 
+                textAlign: 'center',
+                 margin:10
+                 }
+                 }>
+                  Fading in
+                  </Text>
+                  </FadeInView>
+                  </View>
+                  )
+                }
